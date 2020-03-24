6ff3ab666e40d772368b2b7809f71517c36aed6b
Kevin Tou <toukevin@gmail.com>
2020-03-23 18:13:14 -0700

    Added some basic styles to buttons

--- client/src/components/ButtonContainer.js
+++ client/src/components/ButtonContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
 // Components
 import Button from "./Button";
@@ -8,34 +9,50 @@ import Button from "./Button";
 import { drawCard, shuffleDeck } from "../actions";
 
 const ButtonContainer = props => {
-  const hit = e => {
-    e.preventDefault();
-
-    props.drawCard(props.deckID);
-  };
-
-  const shuffle = e => {
-    e.preventDefault();
-
-    props.shuffleDeck(props.deckID);
-  };
-
   return (
-    <>
-      <Button title="Re-Shuffle" action={shuffle} />
-      <Button title="New Hand" />
-      <Button title="Hit" action={hit} />
-      <Button title="Stay" />
-      <Button title="Split" />
-      <Button title="Double" />
-    </>
+    <PlayerActions>
+      <DeckActions>
+        <Button style={{ marginRight: "1rem" }} title="Reset" />
+        <Button style={{ marginRight: "1rem" }} title="Deal" />
+      </DeckActions>
+      <HandActions>
+        <Button style={{ marginLeft: "1rem" }} title="Hit" />
+        <Button style={{ marginLeft: "1rem" }} title="Stay" />
+        <Button style={{ marginLeft: "1rem" }} title="Split" />
+        <Button style={{ marginLeft: "1rem" }} title="Double" />
+      </HandActions>
+    </PlayerActions>
   );
 };
 
+const PlayerActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0.5rem;
+`;
+
+const DeckActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HandActions = styled.div`
+  display: flex;
+  flex-start: flex-end;
+
+  @media (max-width: 700px) {
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    height: 7rem;
+    width: 50%;
+  }
+`;
+
 const mapStateToProps = ({ gameReducer }) => {
-  const { deckID } = gameReducer;
+  const { deckID, playerHand } = gameReducer;
   return {
-    deckID
+    deckID,
+    playerHand
   };
 };
 
