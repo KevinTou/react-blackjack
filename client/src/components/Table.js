21e172cc763daf858fd6d0752226b835f1fc52f9
Kevin Tou <toukevin@gmail.com>
2020-03-23 18:13:36 -0700

    Changed some of the table styles

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
-import { getNewShuffledDeck } from "../actions";
 
 // Components
 import DeckForm from "./DeckForm";
 import ButtonContainer from "./ButtonContainer";
+
+// Actions
+import { getNewShuffledDeck } from "../actions";
+import { checkTotal } from "../util/game";
 
 const Table = props => {
   console.log(props);
@@ -23,44 +26,90 @@ const Table = props => {
   });
 
   return (
-    <TableContainer>
-      <DeckForm />
-      <p>Cards remaining: {props.remainingCards}</p>
-      <div style={{ display: "flex" }}>
-        {props.playerHand.map(card => {
-          return (
-            <div
-              key={card.code}
-              style={{
-                maxWidth: "15%"
-              }}
-            >
-              <img
-                src={card.images.png}
-                alt={`${card.value} OF ${card.suit}`}
-                style={{ width: "100%" }}
-              />
-            </div>
-          );
-        })}
-      </div>
-
+    <>
+      <TableContainer>
+        <DeckForm />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <p>Cards remaining: {props.remainingCards}</p>
+        </div>
+        <h3>Dealer's Hand</h3>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+          }}
+        >
+          {props.dealerHand.map(card => {
+            return (
+              <div
+                key={card.code}
+                style={{
+                  maxWidth: "15%"
+                }}
+              >
+                <img
+                  src={card.images.png}
+                  alt={`${card.value} OF ${card.suit}`}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <p>
+          Dealer's Total:{" "}
+          {props.dealerHand.length > 1 ? checkTotal(props.dealerHand) : 0}
+        </p>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+          }}
+        >
+          <h3>Player's Hand</h3>
+          {props.playerHand.map(card => {
+            return (
+              <div
+                key={card.code}
+                style={{
+                  maxWidth: "15%"
+                }}
+              >
+                <img
+                  src={card.images.png}
+                  alt={`${card.value} OF ${card.suit}`}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            );
+          })}
+          <p>
+            Player's Total:{" "}
+            {props.playerHand.length > 1 ? checkTotal(props.playerHand) : 0}
+          </p>
+        </div>
+      </TableContainer>
       <ButtonContainer />
-    </TableContainer>
+    </>
   );
 };
 
 const TableContainer = styled.div`
-  background-color: lightgreen;
-  min-height: 70%;
+  background-color: #877f82;
+  min-height: 60%;
+  padding: 2rem;
+  border-radius: 0.5rem;
 `;
 
 const mapStateToProps = ({ gameReducer }) => {
-  const { remainingCards, deckID, playerHand } = gameReducer;
+  const { remainingCards, deckID, playerHand, dealerHand } = gameReducer;
   return {
     remainingCards,
     deckID,
-    playerHand
+    playerHand,
+    dealerHand
   };
 };
 
